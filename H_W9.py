863114fb98e8925ba45e3a2329d77ab85b53d9c0
Za-wg <74114154+Za-wg@users.noreply.github.com>
2021-02-25 20:35:15 +0200

    Update H_W9.py

--- H_W9.py
+++ H_W9.py
@@ -2,32 +2,27 @@ import os
 import json
 import random
 import string
-#
-#
-# def read_txt():
-#     with open("names.txt", "r") as names:
-#         data = names.read().split('\n')
-#         for i, item in enumerate(data):
-#             if item != '\n':
-#                 data[i] = item.split('\t')
-#         return data
-#
-#
-# def get_last_name(data):
-#     last_name_list = [lists[1] for lists in data]
-#     return last_name_list
-#
-#
-# data = read_txt()
-# data = get_last_name(data)
-#
-# print(data)
+
+
+def read_txt():
+    with open("names.txt", "r") as names:
+        data = names.read().split('\n')
+        for i, item in enumerate(data):
+            if item != '\n':
+                data[i] = item.split('\t')
+        return data
+
+
+def get_last_name(data):
+    last_name_list = [lists[1] for lists in data]
+    return last_name_list
+
+
+data = read_txt()
+data = get_last_name(data)
+
+print(data)
 # #####################################2
-
-
-def generate_and_write_json():
-    file_path = os.path.abspath('PyHw/H_W9.json')
-    return file_path
 
 
 def add_last_name_in_file():
@@ -43,14 +38,13 @@ def random_key():
 
 def random_value():
     value = random.choice([int, bool, float])
-    for result in value:
         if value == int:
             random.randint(-100, 100)
         elif value == float:
             random.uniform(0, 1)
         elif value == bool:
             random.choice([False, True])
-    return result
+    return value
 
 
 def random_dict():
