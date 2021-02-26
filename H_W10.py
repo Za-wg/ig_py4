7637afc3c171007890927603b54c5337b5f1f759
Za-wg <74114154+Za-wg@users.noreply.github.com>
2021-02-26 16:45:09 +0200

    Update H_W10.py

--- H_W10.py
+++ H_W10.py
@@ -2,32 +2,35 @@ import json
 import csv
 
 
-def read_file(file_path):
+def read_json_file(file_path):
     with open(file_path,  'r'):
         if ".json" in file_path:
-            text = json.load(file_path)
-            print(text)
-            file_path.close()
-        elif ".cvs" in file_path:
-            text1 = csv.reader(file_path)
-            print(text1)
-            file_path.close()
-        elif ".txt" in file_path:
-            with open(file_path, "r") as txt_file:
-                data = []
+            json.load(file_path)
+    return file_path
+
+
+def read_cvs_file(file_path):
+    with open(file_path, 'r'):
+        if ".cvs" in file_path:
+            csv.reader(file_path)
+    return file_path
+
+
+def read_txt_file(file_path):
+    with open(file_path, "r") as txt_file:
+        if ".txt" in file_path:
+            data = []
             for line in txt_file.readlines():
                 data.append(line)
-            file_path.close()
-        else:
-            print("Unsupported file format")
     return file_path
+
 # ###################################2
 
 
 file_path = '/Users/irinagolakova/PycharmProjects/ig_py4/PyHw/Py9.json'
 
 
-def file_write(file_path, text):
+def file_write_json(file_path):
     with open(file_path, "w") as my_file:
         if ".json" in file_path:
             text = {"People": [
@@ -41,16 +44,21 @@ def file_write(file_path, text):
                 }
                     ]}
             json.dump(text, my_file, indent=2)
-        elif ".csv" in file_path:
+
+
+def file_write_cvs(file_path):
+    with open(file_path, "w") as my_file:
+        if ".csv" in file_path:
             names = ["Имя", "Возраст"]
             file_writer = csv.DictWriter(my_file, delimiter=",", lineterminator="\r", fieldnames=names)
             file_writer.writeheader()
             file_writer.writerow({"Имя": "Саша", "Возраст": "6"})
             file_writer.writerow({"Имя": "Маша", "Возраст": "15"})
             file_writer.writerow({"Имя": "Вова", "Возраст": "14"})
-        elif ".txt" in file_path:
+
+
+def file_write_txt(file_path):
+    with open(file_path, "w") as my_file:
+        if ".txt" in file_path:
             text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
             my_file.write(text)
-        else:
-            print("Unsupported file format")
-    return my_file
